b7c71207896691b2259de2474189465ee12dee5c
pierrelefevre <hello@pierrelf.com>
2023-11-10 13:59:06 +0000

    Fix winequality_balanced_typed feature view malformed for some reason

--- lab1/task2/helpers/cleanup_feature_view_versions.py
+++ lab1/task2/helpers/cleanup_feature_view_versions.py
@@ -1,12 +1,16 @@
 import hopsworks
 
-project = hopsworks.login(project="id2223_pierrelf_emilk2")
-fs = project.get_feature_store()
-fg = fs.get_feature_group(name="winequality", version=1)
-query = fg.select_all()
-feature_view = fs.get_or_create_feature_view(name="winequality",
-                                                version=1,
-                                                description="Read from winequality dataset",
-                                                labels=["quality"],
-                                                query=query)
-feature_view.delete_all_training_datasets()
+groups = ["winequality", "winequality_balanced", "winequality_typed_balanced"]
+
+for group in groups:
+
+    project = hopsworks.login(project="id2223_pierrelf_emilk2")
+    fs = project.get_feature_store()
+    fg = fs.get_feature_group(name=group, version=1)
+    query = fg.select_all()
+    feature_view = fs.get_or_create_feature_view(name=group,
+                                                    version=1,
+                                                    description="Read from winequality dataset",
+                                                    labels=["quality"],
+                                                    query=query)
+    feature_view.delete_all_training_datasets()
