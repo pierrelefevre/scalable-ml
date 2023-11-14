88c72b70217d6d2cfd64c2a713a33b68c39563cd
pierrelefevre <hello@pierrelf.com>
2023-11-14 14:07:54 +0000

    Add image download functionality and update dependencies

--- lab1/task2/inference.py
+++ lab1/task2/inference.py
@@ -1,32 +1,41 @@
 import hopsworks
 import joblib
 import shutil
+import requests
+from PIL import Image
 
 def load_model(project):
     mr = project.get_model_registry()
-    model = mr.get_model("wine_model", version=1)
+    model = mr.get_model("wine_model")
     model_dir = model.download()
     model = joblib.load(model_dir + "/wine_model.pkl")
 
+    return model
+
 def save_wine_image(quality, path):
-    shutil.copyfile(f"./img/{quality}.png", path)
+    url = f"https://raw.githubusercontent.com/pierrelefevre/scalable-ml/main/lab1/task2/img/{quality}.png"
+
+    img = Image.open(requests.get(url, stream=True).raw)            
+    img.save(path)
 
 def main():
     project = hopsworks.login()
     fs = project.get_feature_store()
-    fg = fs.get_feature_group(name="iris", version=1)
-    fv = fs.get_feature_view(name="iris", version=1)
+    fg = fs.get_feature_group(name="winequality_typed_balanced")
+    fv = fs.get_feature_view(name="winequality_typed_balanced")
     dataset_api = project.get_dataset_api()
 
+    print("Reading latest wine quality...")
     batch_data = fv.get_batch_data()
+
+    print("Loading model...")
     model = load_model(project)
 
     y_pred = model.predict(batch_data)
-    predicted_quality = y_pred[0]
+    predicted_quality = y_pred[y_pred.size-1]
     print("Predicted quality: {}".format(predicted_quality))
 
-    df = fg.read() 
-    actual_quality = df.iloc[-1]["quality"]
+    actual_quality = fg.read().iloc[-1]["quality"]
     print("Actual quality: {}".format(actual_quality))
 
     # write image to hopsworks
